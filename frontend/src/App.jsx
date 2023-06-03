599c7d4b7428bfea50c2727aaa64dbee7f440a6b
Inal Mahpud <inalmahpud1@gmail.com>
2023-06-04 04:07:51 +0700

    mengupload projek fullstack icc season 2

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Router from "./router/Router";
+const App = () => {
+  return (
+    <main>
+      <Router />
+    </main>
+  )
+};
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="font-bold text-9xl">home</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
